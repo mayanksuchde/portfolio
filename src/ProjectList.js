e284147aa8359a4244f269c7ff6c24e3e517d609
Mayank Suchde <mayank.suchde.4500@gmail.com>
2019-06-22 22:11:42 -0400

    Add Pop up for project details

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -9,25 +9,28 @@ export default class ProjectList extends Component {
     let projArr=[
       {
         name:"Warbler",
+        shortDesc:"twitter clone",
         description:"It is a mini version of twitter where a User can start posting messages upto 280 characters",
         stack:[],
-        gitLink:"",
+        gitLink:"https://github.com/mayanksuchde/warbler",
         liveDemo:"",
         screenshot:warbImg
       },
       {
         name:"plan-react-app",
+        shortDesc:"twitter clone",
         description:"It is a mini version of twitter where a User can start posting messages upto 280 characters",
         stack:[],
         gitLink:"https://github.com/mayanksuchde/plan-react-app",
         liveDemo:"",
-        screenshot:planImg
+        screenshot: planImg
       },
       {
-        name:"RGB game",
-        description:"It is a mini version of twitter where a User can start posting messages upto 280 characters",
+        name:"Colors App",
+        shortDesc:"Clone of Material UI Colors",
+        description:"orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets ",
         stack:[],
-        gitLink:"",
+        gitLink:"https://github.com/mayanksuchde/colors-app",
         liveDemo:"",
         screenshot:rgbImg
       }
