464f133c362c85b96b1653a193ef77c35af9c85e
Mayank Suchde <mayank.suchde.4500@gmail.com>
2019-11-20 12:17:26 -0500

    mobile parallax fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
 //import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
-import Rellax from 'rellax';
+
 import Home from './Home';
 import About from './About';
 import Skills from './Skills';
 import ProjectList from  './ProjectList';
 import Contact from './Contact';
-import {ReactComponent as SmallSq} from './assets/bg/smallrecs.svg'
-import {ReactComponent as MedSq} from './assets/bg/medrecs.svg'
-import {ReactComponent as LargeSq} from './assets/bg/largerecs.svg'
+import Background from './Background';
+
 import './Styles/App.scss';
 
 
 class App extends Component {
+  state = {
+    isMobile: false,
+  
+  }
+
+  
   componentDidMount=()=>{
-    var rellax = new Rellax('.rellax');
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+  resize() {
+    this.setState({isMobile:window.innerWidth<525});
+  }
+
+
+  componentWillUnmount=()=>{
+    window.removeEventListener('resize', this.throttledHandleWindowResize);
   }
   render() {
-   
+   const {isMobile}=this.state;
     return (
         <div className="App">
           <div className="bg">
-            <div className="parallax p1 rellax" data-rellax-speed="30">
-              <SmallSq />
+            { isMobile ? 
+                (
+                  <div>
+                    <Background top={0} />
+                    <Background top={1} />
+                    <Background top={2} />
+                  </div>
+                ) 
+                : 
+                (<Background top={0} />) }
+             
+            {/* may be try to put the top padding as a props so we can use it multiple times     */}
+            <div className='main'>
+              <Home  />
+              <About />
+              <Skills />
+              <ProjectList />
+              <Contact />
             </div>
-            <div className="parallax p2 rellax" data-rellax-speed="20">
-              <MedSq />
-            </div>
-            <div className="parallax p3 rellax" data-rellax-speed="10">
-              <LargeSq />
-            </div>
-          </div>
-              
-          <div className='main'>
-            <Home  />
-            <About />
-            <Skills />
-            <ProjectList />
-            <Contact />
           </div>
           {/* </Parallax> */}
           
