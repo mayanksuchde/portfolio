a5feff3ccab6120a7677abcf025894a42aaaa4be
Mayank Suchde <mayank.suchde.4500@gmail.com>
2020-03-10 19:41:53 -0400

    npm update

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3'
 
 
 export default class About extends Component {
-//   componentDidMount(){
+   componentDidMount(){
 //     var width = 450,
 //     height = 450,
 //     margin = 40;
@@ -36,7 +36,7 @@ export default class About extends Component {
 //         .attr("stroke","black")
 //         .style("stroke-width","2px")
 //         .style("opacity",0.7)           
-//}
+}
 
   render() {
     return (
