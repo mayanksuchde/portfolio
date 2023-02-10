56cd157d729a66f00686187117d4ea83a25193d2
Mayank Suchade <mayank.suchde.4500@gmail.com>
2023-02-10 13:26:27 -0500

    form funtional

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Button, Box, TextField} from '@mui/material';
-import resume from '../assets/resume.pdf';
-import { OpenInNewRounded, Send } from "@mui/icons-material";
+//import resume from '../assets/resume.pdf';
+import { Send } from "@mui/icons-material";
 import './Contact.scss';
 import useInputState from '../hooks/useInputState';
-import { purple, red } from '@mui/material/colors';
 
 function Contact() {
-    const [name,handleName,resetName]=useInputState("");
-    const [email,handleEmail,resetEmail]=useInputState("");
-    const [subject,handleSubject,resetSubject]=useInputState("");
-    const [message,handleMessage,resetMessage]=useInputState("");
-    const primary = purple[200];
-    const handleSendMessage=(e) => {
-        e.preventDefault();
-        //console.log(name,message,subject,email)
-        //handle submission here
+    const [name,handleName]=useInputState("");
+    const [email,handleEmail]=useInputState("");
+    const [subject,handleSubject]=useInputState("");
+    const [message,handleMessage]=useInputState("");
+    const [result, setResult] = useState("Send Message");
+
+  const submitMessage = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "c09e02db-c56b-4ec0-a9b7-005b56042331");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult(res.message);
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
     }
+  };
     return ( 
         <div className="contact page">
             <h2>Get in Touch</h2>
@@ -29,33 +44,35 @@ function Contact() {
                 }}
                 noValidate
                 autoComplete="off"
+                onSubmit={submitMessage}
                 >
-                <TextField className="input" fullWidth id="outlined-basic" label="Name" variant="outlined" value={name} onChange={handleName} />
-                <TextField className="input" fullWidth id="outlined-basic" label="Email" variant="outlined" value={email} onChange={handleEmail} />
-                <TextField className="input" fullWidth id="outlined-basic" label="Subject" variant="outlined" value={subject} onChange={handleSubject} />
+                <TextField className="input" name="name" fullWidth id="outlined-basic" label="Name" variant="outlined" value={name} onChange={handleName} />
+                <TextField className="input" name="email" fullWidth id="outlined-basic" label="Email" variant="outlined" value={email} onChange={handleEmail} />
+                <TextField className="input" name="subject" fullWidth id="outlined-basic" label="Subject" variant="outlined" value={subject} onChange={handleSubject} />
                 <TextField 
                     className="input"
                     fullWidth
                     id="outlined-basic" 
                     label="Message" 
+                    name="message"
                     variant="outlined" 
                     multiline rows={4} 
                     value={message}
                     onChange={handleMessage}
                 />
-                <Button type="submit" variant='contained' onClick={e=>handleSendMessage(e)} >
-                    Send Message  
+                <Button type="submit" variant='contained' >
+                    {result}  
                     <Send />
                 </Button>
             </Box>
-                <div className='resume'>
+                {/* <div className='resume'>
                     <form target="_blank" action={resume} method="get">
                         <Button variant='contained' type="submit">
                             View Resume
                             <OpenInNewRounded />
                         </Button>
                     </form>
-                </div>
+                </div> */}
             </div>
         </div> 
     );
